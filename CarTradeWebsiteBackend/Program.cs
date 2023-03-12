0036c6b3d6009e06f25f1f7fb4d2391d2430c420
AlexanderSvl <aleksander.svilarovv@gmail.com>
2023-03-12 20:54:22 +0200

    Nothing major

--- CarTradeWebsiteBackend/Program.cs
+++ CarTradeWebsiteBackend/Program.cs
@@ -76,3 +76,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
--- CarTradeWebsiteFrontend/src/app/components/search/search.component.ts
+++ CarTradeWebsiteFrontend/src/app/components/search/search.component.ts
@@ -9,59 +9,14 @@ import { CarResponseModel } from 'src/app/models/carResponseModel';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
-  startYear = '';
-  endYear = '';
-  selectedOption = "";
-  searchTerms = "";
   cars: CarResponseModel[] = [];
 
   constructor(private http: HttpClient, public searchService: SearchService) {}
 
   onSelect(value: string){
-    this.selectedOption = value;
   }
 
   onSubmit(){
-    if(this.startYear != "" && this.endYear != ""){
-      this.searchService.searchYearOfProduction(this.startYear, this.endYear).subscribe((data: any) => {
-        for (let i = 0; i < data.length; i++) {
-          this.cars.push(data[i]);
-        }
-
-        if(this.selectedOption = "Title"){
-          this.searchService.searchTitle(this.searchTerms).subscribe((res: any) => {
-
-          })
-        }
-        else if(this.selectedOption = "Description"){
-          this.searchService.searchDescription(this.searchTerms).subscribe((res: any) => {
-            
-          })
-        }
-        else if(this.selectedOption = "Car Make"){
-          this.searchService.searchCarMake(this.searchTerms).subscribe((res: any) => {
-            
-          })
-        }
-        else if(this.selectedOption = "Car Model"){
-          this.searchService.searchCarModel(this.searchTerms).subscribe((res: any) => {
-            
-          })
-        }
-        else if(this.selectedOption = "Engine Displacement"){
-          this.searchService.searchEngineDisplacement(this.searchTerms).subscribe((res: any) => {
-            
-          })
-        }
-        else if(this.selectedOption = "TransmissionType"){
-          this.searchService.searchTransmissionType(this.searchTerms).subscribe((res: any) => {
-            
-          })
-        }
-
-      });
-    }
-
-    console.log(this.cars)
+    
   }
 }
